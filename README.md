d6833b9ddd4784b07e053a1b55ebf68310fb4e55
Jesús Antonio García Valadez <jesus.garciav@me.com>
2020-09-11 22:03:55 -0500

    feat: initial commit
    Signed-off-by: Jesús Antonio García Valadez <jesus.garciav@me.com>

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# worksome-freelance
-Worksome Professional Freelance profile
+# woksome-freelance
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+
+# generate static project
+$ yarn generate
+```
+
+For detailed explanation on how things work, check out [Nuxt.js docs](https://nuxtjs.org).
---
+++ nuxt.config.js
@@ -0,0 +1,109 @@
+import { join } from 'path'
+
+export default {
+  /*
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
+  mode: 'universal',
+  /*
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
+  target: 'static',
+  /*
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', type: 'https://rsms.me/inter/inter.css' },
+    ],
+  },
+  /*
+   ** Global CSS
+   */
+  css: ['~/assets/main.css'],
+  /*
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
+  plugins: ['@/plugins/vue-fragment.js'],
+  /*
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
+  components: true,
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    '@nuxt/typescript-build',
+    // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
+    '@nuxtjs/tailwindcss',
+    'nuxt-purgecss',
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    // Doc: https://github.com/nuxt/content
+    '@nuxt/content',
+  ],
+  /*
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
+  axios: {},
+  /*
+   ** Content module configuration
+   ** See https://content.nuxtjs.org/configuration
+   */
+  content: {},
+  /*
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+  build: {
+    cssSourceMap: true,
+    postcss: {
+      plugins: {
+        'postcss-import': {},
+        autoprefixer: {},
+        tailwindcss: join(__dirname, 'tailwind.config.js'),
+        cssnano: {
+          preset: [
+            'default',
+            {
+              discardComments: { removeAll: true },
+            },
+          ],
+        },
+      },
+      preset: {
+        autoprefixer: {
+          grid: true,
+        },
+      },
+      order: (names, presets) => presets.cssnanoLast(names),
+    },
+  },
+  router: {
+    linkActiveClass: '',
+    linkExactActiveClass: '',
+  },
+}
