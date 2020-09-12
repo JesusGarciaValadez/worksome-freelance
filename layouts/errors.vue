f897abf81d183435853e29a2c5a687e72442cc56
Jesús Antonio García Valadez <jesus.garciav@me.com>
2020-09-12 18:24:21 -0500

    feat: add new layouts
    Signed-off-by: Jesús Antonio García Valadez <jesus.garciav@me.com>

---
+++ layouts/errors.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="lg:pl-32 mt-12">
+    <div class="max-w-md">
+      <h1 class="text-2xl font-extrabold text-gray-900 mb-4">
+        Page not found!
+      </h1>
+      <div class="text-lg text-grey-900 leading-normal spaced-y-6">
+        <h2 class="text-6xl font-bold">404!</h2>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+export default {
+  props: ['error']
+};
+</script>
