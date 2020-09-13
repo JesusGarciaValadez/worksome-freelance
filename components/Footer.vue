9f0786201a038c80f612aee07219fb4d76f51465
Jesús Antonio García Valadez <jesus.garciav@me.com>
2020-09-13 16:02:07 -0500

    feat: add links to another pages
    Signed-off-by: Jesús Antonio García Valadez <jesus.garciav@me.com>

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,14 @@
   <div>
     <div class="max-w-screen-xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
       <div class="mt-8">
-        <p class="text-center text-base leading-6 text-cool-gray-400">&copy;2020 Worksome ApS. hej@worksome.dk. FAQ Terms Privacy Contact</p>
+        <p class="text-center text-base leading-6 text-cool-gray-400">
+          &copy;2020 Worksome ApS.
+          <a href="mailto:hej@worksome.dk">hej@worksome.dk</a>.
+          <nuxt-link to="/pages/faq">FAQ</nuxt-link>
+          <nuxt-link to="/pages/terms">Terms</nuxt-link>
+          <nuxt-link to="/pages/privacy">Privacy</nuxt-link>
+          <nuxt-link to="/pages/contact">Contact</nuxt-link>
+        </p>
       </div>
     </div>
   </div>
